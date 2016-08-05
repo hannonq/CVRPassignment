ea96eacf0a39257d83ab49aa9be701ae1b6067ec
Junior Rhis <juniorrhis1@gmail.com>
2016-08-05 15:01:44 -0300

    Script de testes.

--- src/main/java/br/com/ufop/constants/K.java
+++ src/main/java/br/com/ufop/constants/K.java
@@ -3,7 +3,7 @@ package br.com.ufop.constants;
 public class K {
 	public static final String INSTANCES_PATH = "instances/";
 	public static final int POPULATION_AMOUNT = 100;
-	public static final int GENERATIONS_AMOUNT = 400;
+	public static final int GENERATIONS_AMOUNT = 300;
 	public static final double MUTATION_RATE = 0.04;
 	
 	public static final double REPRODUCTION_MODIFYING_PERCENTAGE = 0.05;
